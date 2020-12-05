d012cc81ad171c78ea3523488c0ed281e0e4c88d
Kunal Mahato <kunalmahato11@gmail.com>
2020-12-05 20:04:02 +0530

    Changed the 2nd method using HashMap

--- dsa-cp-1/Check-Anagrams/code/Method-2.cpp
+++ dsa-cp-1/Check-Anagrams/code/Method-2.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 
 bool checkAnagram(string s1, string s2)
-{
+{   
+    // If length of the str are not same then they are not anagrams
     if (s1.length() != s2.length())
         return false;
 
     int count = 0;
-
-    // Take sum of all characters of first String
-    for (int i = 0; i < s1.length(); i++)
-    {
-        count += s1[i];
-    }
-
-    // Subtract the Value of all the characters of second string
-    for (int i = 0; i < s2.length(); i++)
-    {
-        count -= s2[i];
-    }
-
-    // If Count = 0 then they are anagram
-    if (count == 0)
-        return true;
-
-    return false;
+    
+    // Map to store the count of each character
+    map<char, int> m;
+	
+    // Insert string 1 character into map
+	for (auto i : s1) {
+		m[i]++;
+	}
+	
+    // Subtract the string 2 characters from the map
+    // If the count become less than 0 that means 
+    // that character is occurring more number of times in string 2
+    // which means they are not anagrams
+	for (auto i : s2) {
+		m[i]--;
+		if(m[i] < 0) {
+			return false;
+		}
+	}
+	
+	return true;
 }
 
 int main()
