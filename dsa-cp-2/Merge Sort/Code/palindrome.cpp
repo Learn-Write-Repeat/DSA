0f090e6986966d4dc8688f3c99ac2a502a7d00f7
akrish4 <ananthakrishnan20121999@gmail.com>
2020-12-06 14:12:30 +0530

    updated commit

--- dsa-cp-2/Merge Sort/Code/palindrome.cpp
+++ dsa-cp-2/Merge Sort/Code/palindrome.cpp
@@ -1,69 +1,99 @@
-#include<bits/stdc++.h>
+// C++ program for Merge Sort
+#include <iostream>
 using namespace std;
 
-class Node {
-public:
-  int data;
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+	int n1 = m - l + 1;
+	int n2 = r - m;
 
-  Node(int d){
-    data = d;
-  }
-  Node *ptr;
-};
+	// Create temp arrays
+	int L[n1], R[n2];
 
-bool isPalindrome(Node* head){
-  // This pointer will allow the first traversal
-  // of the linked list
-  Node* next= head;
-  // Declare a stack
-  stack <int> s;
+	// Copy data to temp arrays L[] and R[]
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
 
-  // Traverse the linked list and add its elements
-  // to the stack
-  while(next != NULL){
-    s.push(next->data);
-    next = next->ptr;
-  }
+	// Merge the temp arrays back into arr[l..r]
 
-  // Iterate the linked list again and
-  // check by each element with the stack
-  while(head != NULL ){
-    int i=s.top();
+	// Initial index of first subarray
+	int i = 0;
 
-    if(head -> data != i){
-      return false;
-    }
-    // Move to the next element in stack and the list
-    s.pop();
-    head=head->ptr;
-  }
+	// Initial index of second subarray
+	int j = 0;
 
-return true;
+	// Initial index of merged subarray
+	int k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// L[], if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// R[], if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 }
 
-// Driver Code
-int main(){
-
-  Node one =  Node(1);
-  Node two = Node(3);
-  Node three = Node(5);
-  Node four = Node(3);
-  Node five = Node(1);
-
-  // Initialize the pointers of the Linked List
-  five.ptr = NULL;
-  one.ptr = &two;
-  two.ptr = &three;
-  three.ptr = &four;
-  four.ptr = &five;
-  Node* temp = &one;
-
-
-  // Call function with head of the linked list
-  int result = isPalindrome(&one);
-  if(result == 1)
-    cout<<"Linked list is a palindrome\n";
-  else
-    cout<<"Linked list is NOT a palindrome\n";
-
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(int arr[],int l,int r){
+	if(l>=r){
+		return;//returns recursively
+	}
+	int m = (l+r-1)/2;
+	mergeSort(arr,l,m);
+	mergeSort(arr,m+1,r);
+	merge(arr,l,m,r);
 }
+
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << A[i] << " ";
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Given array is \n";
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	cout << "\nSorted array is \n";
+	printArray(arr, arr_size);
+	return 0;
+}
+
+// This code is contributed by Mayank Tyagi
